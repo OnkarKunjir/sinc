52f6fedb1ed79f1aebe48175dce1b2f6b1f9fd59
Onkar Kunjir <onkarkunjir8@gmail.com>
2020-07-18 11:40:45 +0530

    finally adding option to share pdf.

--- lib/export_doc.dart
+++ lib/export_doc.dart
@@ -3,6 +3,7 @@ import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import './file_operations.dart';
 import 'dart:io';
+import 'package:esys_flutter_share/esys_flutter_share.dart';
 
 class ExportDoc {
   final FileOperations fileOperations;
@@ -15,7 +16,8 @@ class ExportDoc {
         '${this.fileOperations.documentDirectory.path}/${this.docName}/');
 
     for (String p in imagePaths) {
-      final image = PdfImage.file(
+      print('adding image $p');
+      dynamic image = PdfImage.file(
         pdf.document,
         bytes: File(p).readAsBytesSync(),
       );
@@ -23,19 +25,21 @@ class ExportDoc {
         pw.Page(
           build: (pw.Context context) {
             return pw.Center(
-              child: pw.Image(image),
+              child: pw.Image(image, fit: pw.BoxFit.cover),
             ); // Center
           },
         ),
       );
-
-      final file = File('${this.fileOperations.exportDir.path}/$docName.pdf');
-      await file.writeAsBytes(pdf.save());
-      final l = this.fileOperations.listDir(this.fileOperations.exportDir.path);
-      for (var i in l) {
-        print('Export dir content $i');
-      }
-      return;
     }
+
+    File file = File('${this.fileOperations.exportDir.path}/$docName.pdf');
+    file = await file.writeAsBytes(pdf.save());
+    print('created pdf :- ${file.path}');
+    final bytes = file.readAsBytesSync();
+
+    await Share.file(
+        'esys image', 'esys.pdf', bytes.buffer.asUint8List(), 'application/pdf',
+        text: 'My optional text.');
+    return;
   }
 }
